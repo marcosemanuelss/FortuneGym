dd50934b9a9fc87a71bd5fcfa47549c947660f1e
Ana <ameloq@gmail.com>
2013-10-22 11:31:55 -0300

    ajustes css + Nova tela (Consulta)

---
+++ SAcademia.Web/Administrativo/ConsultaUsuario.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterPageFile="~/Site.Master" CodeBehind="ConsultaUsuario.aspx.cs" Inherits="SFF.Web.Administrativo.ConsultaUsuario" %>
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+        <div class="conteudo">
+            <div class="cabecalho">
+                <div class="seta-right"></div>
+                <h2>Usuários do Sistema</h2>
+            </div>
+            <div class="bg-tabela">
+                <div class="filtrar">
+                    <label>Digite um nome ou cpf para pesquisa:</label>
+                    <asp:TextBox ID="txtPesquisa" runat="server"/>
+                    <asp:Button ID="btnPesquisar" ToolTip="Pesquisar" runat="server" Text="Pesquisar" Cssclass="buttons"/>
+                    <asp:Button ID="btnLimpar" ToolTip="Limpar" Cssclass="buttons" runat="server" Text="Limpar"/>
+                </div>
+                <asp:GridView ID="gvConsulta" CssClass="tabela" runat="server">
+                
+                </asp:GridView>
+            </div>
+        </div>
+</asp:Content>
---
+++ SAcademia.Web/Administrativo/ConsultaUsuario.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+namespace SFF.Web.Administrativo
+{
+    public partial class ConsultaUsuario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CarregaGV();
+        }
+
+        protected void CarregaGV()
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.Add("Login", System.Type.GetType("System.String"));
+            dt.Columns.Add("Nome", System.Type.GetType("System.String"));
+            dt.Columns.Add("E-mail", System.Type.GetType("System.String"));
+            dt.Columns.Add("Academia", System.Type.GetType("System.String"));
+            dt.Columns.Add("Situação", System.Type.GetType("System.String"));
+            dt.Columns.Add("Ação", System.Type.GetType("System.String"));
+            dt.Columns.Add("Resetar Senha", System.Type.GetType("System.String"));
+            dt.Columns.Add("Editar", System.Type.GetType("System.String"));
+            dt.Columns.Add("Excluir", System.Type.GetType("System.String"));
+
+            for (int i = 1; i < 10; i++)
+            {
+                dt.Rows.Add(new String[] {"Nome Teste", "Teste da Silva", "testedasilva@email.com.br", "img", "Inativo", "img", "img", "img", "img" });
+            }
+
+            gvConsulta.DataSource = dt;
+            gvConsulta.DataBind();
+        }
+    }
+}
